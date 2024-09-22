43321a8879d3b176775ae68560bf643546bcb3e3
Allovallo <grossco@meta.ua>
2024-09-22 15:28:48 +0300

    styles updated

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { GlobalStyle } from './components/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hello, React</h1>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <h1>Hello, React</h1>
+    </ThemeProvider>
   </React.StrictMode>
 );
