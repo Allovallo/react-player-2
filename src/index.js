e7fc96d5f4b0becf56de5e985a37c8bab2dd813e
Allovallo <grossco@meta.ua>
2024-09-24 10:38:09 +0300

    files downdated

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from './components/GlobalStyle';
-import { Example1 } from './components/Example1';
+
 import './index.css';
-import videos from './videos.json';
+
 // import { Reader } from 'components/Reader/Reader';
 // import publications from 'publications.json';
 
@@ -12,10 +10,6 @@ const theme = {};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Example1 url={videos} />
-      {/* <Reader items={publications} /> */}
-    </ThemeProvider>
+    <h1>Hello, React</h1>
   </React.StrictMode>
 );
